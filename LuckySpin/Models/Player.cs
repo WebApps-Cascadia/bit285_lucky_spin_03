0b1a91296df6ed0f88325d4db66c5637b15cdeb1
Shelby Holmes <shelbynholmes@gmail.com>
2023-01-12 15:30:15 -0800

    Completed in class exercise 1/12/23

--- LuckySpin/Models/Player.cs
+++ LuckySpin/Models/Player.cs
@@ -4,7 +4,8 @@ namespace LuckySpin.Models
 	public class Player
 	{
 		//TODO: Use data-binding information from Index.cshtml to create appropriate properties
-
+		public int Luck { get; set; }
+		public String FirstName { get; set; }
 	}
 }
 
