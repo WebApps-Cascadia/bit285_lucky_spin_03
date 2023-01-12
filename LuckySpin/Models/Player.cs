2ec0f63b87a73cb9f170a1e35f4aa9bc24c326f0
Noah Ku <thenoahku@gmail.com>
2023-01-12 14:53:36 -0800

    testing

--- LuckySpin/Models/Player.cs
+++ LuckySpin/Models/Player.cs
@@ -4,6 +4,8 @@ namespace LuckySpin.Models
 	public class Player
 	{
 		//TODO: Use data-binding information from Index.cshtml to create appropriate properties
+		string FirstName { get; set; }
+		int Luck { get; set; }
 
 	}
 }
